f1c7624e6d65f27c4bb2928ba7b896f9784ba105
Mike Morgan <mikemorgan@blueyonder.co.uk>
2017-07-10 19:55:11 +0100

    Added some project description files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'pycscrape',
+    packages = ['pycscrape'],
+    version = '0.01',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Initial pre-release',
+    author = 'Mike Morgan',
+    author_email = 'mikemorgan@blueyonder.co.uk',
+    url = 'https://github.com/mikemorgan68/pycscrape.git',
+    download_url = 'https://github.com/mikemorgan68/pycscrape/archive/0.00.tar.gz',
+    keywords = ['C', 'source'],
+    classifiers = [],
+)
+
